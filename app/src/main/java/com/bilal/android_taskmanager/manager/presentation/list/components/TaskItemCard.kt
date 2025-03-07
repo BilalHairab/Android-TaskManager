d97eb99eb4689a9e92ec97eeb50bc9141cfe71fc
Bilal Ibrahim Hairab <34271874+BilalHairab@users.noreply.github.com>
2025-03-07 23:44:34 +0400

    Add completed/pending icons

--- app/src/main/java/com/bilal/android_taskmanager/manager/presentation/list/components/TaskItemCard.kt
+++ app/src/main/java/com/bilal/android_taskmanager/manager/presentation/list/components/TaskItemCard.kt
@@ -24,11 +24,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.bilal.android_taskmanager.R
 import com.bilal.android_taskmanager.manager.domain.Task
 import com.bilal.android_taskmanager.manager.domain.TaskPriority
 import com.bilal.android_taskmanager.manager.presentation.models.getSurfaceColor
@@ -70,7 +73,7 @@ fun TaskItemCard(
             color = MaterialTheme.colorScheme.primary
         ),
         colors = CardDefaults.cardColors(
-            containerColor = task.priority.getSurfaceColor(),
+            containerColor = if(task.completed) MaterialTheme.colorScheme.surfaceContainer else task.priority.getSurfaceColor(),
             contentColor = contentColor
         )
     ) {
@@ -78,7 +81,7 @@ fun TaskItemCard(
             .fillMaxHeight()
             .padding(vertical = 8.dp)) {
             Icon(
-                imageVector = Icons.Rounded.AddCircle,
+                imageVector = ImageVector.vectorResource(id = if (task.completed) R.drawable.ic_completed else R.drawable.ic_pending),
                 contentDescription = if (task.completed) "Completed" else "Incomplete",
                 modifier = Modifier
                     .size(75.dp)
